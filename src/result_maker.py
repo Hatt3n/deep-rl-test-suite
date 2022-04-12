ca8db6ebe137558670a385f5420c9979cf25fadb
Daniel <dansah@kth.se>
2022-04-12 13:50:19 +0200

    Improve documentation

--- src/result_maker.py
+++ src/result_maker.py
@@ -7,6 +7,11 @@ Added by @dansah
 
 def add_architecture(html_str, arch_name, diagrams):
     """
+    Adds a header for an architecture and image tags referring to the
+    result diagrams provided.
+    html_str (string): The HTML-string to append the result to.
+    arch_name (string): Name of the architecture.
+    diagrams (dict): { "diagram_name1": "diagram_filepath1", ... }.
     """
     additional_str = """<h3>%s</h3>""" % arch_name
     for diagram_name in diagrams:
@@ -14,8 +19,14 @@ def add_architecture(html_str, arch_name, diagrams):
 
     return html_str + additional_str
 
+
 def add_environment(html_str, env_name, architecture_diagrams):
     """
+    Adds a header for an environment. Calls add_architecture for
+    every architecture used with the enivronment.
+    html_str (string): The HTML-string to append the result to.
+    env_name (string): The name of the environment.
+    architecture_diagrams (dict): { "arch_name1": { "diagram_name1": "diagram_filepath1", ... }, ... }
     """
     additional_str = """<h2>%s</h2>""" % env_name
     for arch_name in architecture_diagrams:
@@ -23,10 +34,11 @@ def add_environment(html_str, env_name, architecture_diagrams):
 
     return html_str + additional_str
 
-def make_html(output_file_str, environment_architecture_diagrams):
+
+def make_html(output_file_str, environment_architecture_data):
     """
     output_file_str (string): Full filepath to the resulting output file.
-    environment_architecture_diagrams (dict): Should have the following format:
+    environment_architecture_data (dict): Should have the following format:
         {
             "environment_name1": {
                 "arch_name1": {
@@ -43,8 +55,8 @@ def make_html(output_file_str, environment_architecture_diagrams):
     html_str = """<html><head></head><body>"""
 
     # Add content
-    for env_name in environment_architecture_diagrams:
-        html_str = add_environment(html_str, env_name, environment_architecture_diagrams[env_name])
+    for env_name in environment_architecture_data:
+        html_str = add_environment(html_str, env_name, environment_architecture_data[env_name])
 
     # Finish up
     html_str += "</body></html>"
