14945df14110ee2a7d0e6adc0c7b17ae67f26895
Daniel <dansah@kth.se>
2022-05-04 18:16:19 +0200

    Add return

--- src/custom_envs/env_util.py
+++ src/custom_envs/env_util.py
@@ -28,6 +28,7 @@ class DiscretizingEnvironmentWrapper(gym.core.Env):
         self.env = env_fn()
         if self.env is None:
             print("ERROR: Failed to initialize discrete environment, env_fn call returned None.")
+            return
 
         # Set values related to discretization
         self.n = n if n % 2 == 1 else n+1 # n should be odd, to include 0 (all action values will be of equal distance to each other)
