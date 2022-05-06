bba73ebf8c1544b9f7a75ec9931d330a7c2849be
Daniel <dansah@kth.se>
2022-05-06 16:59:18 +0200

    Add new experiment

--- src/exp_config.py
+++ src/exp_config.py
@@ -47,6 +47,11 @@ def get_experiment(exp_name):
         algorithms_to_use = ["ddpg", "a2c", "a2c_s", "ppo"]
         architecture_to_use = ["64_64_relu"]
         seeds = [0, 10, 100, 1000]
+    elif exp_name == "pbrs_comp2":
+        envs_to_use = ["furuta_pbrs2", "furuta_pbrs3"]
+        algorithms_to_use = ["dqn", "reinforce", "ddpg", "a2c", "a2c_s", "ppo"]
+        architecture_to_use = ["64_64_relu"]
+        seeds = [0, 10, 100, 1000]
     elif exp_name == "qube2_sim":
         envs_to_use = ["qube2_sim"]
         algorithms_to_use = ["ddpg"]
